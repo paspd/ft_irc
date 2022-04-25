5da2b01ad6a2c61586c9ee1e80e911bb33c87b3c
paspd <leoir1807@gmail.com>
2022-04-25 14:07:32 +0200

    poush

--- srcs/Commands/INVITE.cpp
+++ srcs/Commands/INVITE.cpp
@@ -18,7 +18,7 @@ void Server::invite(std::vector<std::string> command, int actualClient) {
 			throw Exception::ERR_USERONCHANNEL(clientName, channelName);
 	}
 	if (_channels[chanIndex].getMode('i', _clients[actualClient]))
-		if (!_channels[chanIndex].getMode('C', _clients[actualClient]) || !_channels[chanIndex].getMode('o', _clients[actualClient]))
+		if (!_channels[chanIndex].getMode('C', _clients[actualClient]) && !_channels[chanIndex].getMode('o', _clients[actualClient]))
 			throw Exception::ERR_CHANOPRIVSNEEDED(channelName);
 	_clients[clientIndex].addInvitation(channelName);
 	sendMessage(_clients[actualClient].getClientSocket(), RPL_INVITING(_clients[actualClient].createClientPrompt(), channelName, clientName, _clients[actualClient].getClientNickname()));
